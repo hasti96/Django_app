be797492edf048969f2639b519412abc281076c7
Hastimal Paliwal <hastimal3620@ISGMGORD064>
2019-07-09 18:56:12 +0530

    Polls Done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gx)rku&5!iau+wgj4iik3gj!#_pinzk5=mo99wyv6ubzn$dkz1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.88.77','localhost','127.0.0.1','192.168.81.86']
+ALLOWED_HOSTS = ['192.168.88.77','localhost','127.0.0.1','192.168.81.86','192.168.81.83']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
